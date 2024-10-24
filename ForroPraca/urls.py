d6cd6e5e2d7359ead0999958d1dbc23b7e5c305c
SilviaDebussi <silviadebussi2@gmail.com>
2024-10-24 16:04:58 -0300

    alt na url

--- ForroPraca/urls.py
+++ ForroPraca/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from aplic import views  # importe sua view
+from django.urls import path, include
+from aplic import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('aplic.urls')),
     path('usuarios/', views.listar_usuarios, name='listar_usuarios'),
     path('usuarios/novo/', views.criar_usuario, name='criar_usuario'),
     path('usuarios/editar/<int:id>/', views.atualizar_usuario, name='atualizar_usuario'),
